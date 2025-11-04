6cb86974cd6464a8b5898a612c93f9a65af1550d
Gabriel <gabriel_aires@estudante.sesisenai.org.br>
2025-11-04 19:46:07 -0300

    vent

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import redis 
 
-r = redis.Redis(host='10.1.69.134', port=6379, db=0)
+# Mude sua linha de conexão para isto:
+r = redis.Redis(host='10.1.69.134', port=6379, db=0, decode_responses=True)
 r.ping()
 
 # cria uma sala
 
-r.hset("2367", "p1", "")
+r.hset("2367", "p1", "2")
 r.hset("2367", "p2", "1")
 
 # mostra resultados
 dados = r.hgetall("2367")
+print(dados)
