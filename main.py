f3a42b57dd3f48be72529dc88d933ce2c544cde7
Gabriel <gabriel_aires@estudante.sesisenai.org.br>
2025-11-04 19:23:34 -0300

    arquivos

---
+++ main.py
@@ -0,0 +1,12 @@
+import redis 
+
+r = redis.Redis(host='10.1.68.172', port=6379, db=0)
+r.ping()
+
+# cria uma sala
+
+# r.hset("2367", "p1", "")
+r.hset("2367", "p2", "1")
+
+# mostra resultados
+dados = r.hgetall("2367")
